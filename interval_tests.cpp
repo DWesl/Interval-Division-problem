e3ec03bda8cd8ffc956fa6a0ec258e6a34d80068
DWesl <22566757+DWesl@users.noreply.github.com>
2023-08-02 07:05:38 -0400

    ENH: Use the fastest implementation in the indicated file.
    I was told to put the function divide() in interval_tests.cpp
    I got a bit carried away and put the implementation and tests in
    files that matched the naming scheme for the TimeInterval implementation,
    except for the extensions.

--- interval_tests.cpp
+++ interval_tests.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+constexpr TimeInterval ZERO(0, 0, 0);
+constexpr inline long NUM_RANDOM_TESTS = 250;
+
+constexpr long SECONDS_PER_HOUR = 3'600;
+constexpr long HOURS_PER_DAY = 24;
+constexpr long SECONDS_PER_DAY = SECONDS_PER_HOUR * HOURS_PER_DAY;
+
 /**
  * \brief Divides one time interval by another
  *
@@ -18,18 +25,70 @@ void divide(const TimeInterval &numerator,
 {
 	quotient = 0;
 	remainder = numerator;
+	bool negateResult = false;
+
+	// Handle negative numerator and denominator
+	if (numerator < ZERO) {
+		negateResult = ~negateResult;
+		numerator *= -1;
+	}
+	if (denominator < ZERO) {
+		negateResult = ~negateResult;
+		denominator *= -1;
+	}
+	if (denominator == ZERO) {
+		throw std::invalid_argument("Zero division error");
+	}
 
 	//
-	// The simplest algorithm...
+	// Long division, roughly as taught in grade school
 	//
-	while (remainder >= denominator) {
-		remainder -= denominator;
-		quotient++;
+	if (denominator <= numerator) {
+		// First, find out what digit to start with.
+		// Humans do this with heuristics analogous to natural logarithms
+		// This checks successive powers of ten until one is too big
+		// then steps back one
+		long place_value = DIGIT_BASE;
+		while (denominator * place_value < remainder) {
+		  place_value *= DIGIT_BASE;
+		  if (place_value > std::numeric_limits<long>::max() / DIGIT_BASE) {
+		    throw std::overflow_error("Quotient doesn't fit in result type");
+		  }
+		}
+		place_value /= DIGIT_BASE;
+
+		// Now the core loop of long division
+		// For each place (thousands, hundreds, tens, ones)
+		// find what digit goes there, then go on to the next
+		// This just checks each possible digit in sequence
+		// Binary search might be faster
+		while (remainder > denominator) {
+			long here_digit = 0;
+			for (here_digit = 0; here_digit < DIGIT_BASE; ++here_digit) {
+			  if (here_digit * place_value * denominator > remainder) {
+			    // We've found a digit which is too big; Go back
+			    // to the previous one, which keeps the
+			    // remainder positive.
+			    --here_digit;
+			    break;
+			  }
+			}
+			// Add the new digit to the quotient, subtract the
+			// now-explained part from the working remainder.
+			// Fewer working digits means faster normalize()
+			quotient += here_digit * place_value;
+			remainder -= here_digit * place_value * denominator;
+			place_value /= DIGIT_BASE;
+		}
 	}
 
 	//
 	// Are there edge cases that aren't handled by the simple algorithm?
 	//
+	if (negateResult) {
+		quotient *= -1;
+		remainder *= -1;
+	}
 
 	//
 	// Are there algorithms that can arrive at the answer much more efficiently?
